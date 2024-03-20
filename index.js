5f2a9f3ee71789a7115757e6c4748cb95bbd3103
nqduy1805 <tnu1805@gmail.com>
2024-03-20 16:00:38 +0700

    u

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-function display(name){
-    console.log(name)
-    return name;
-}
-display("api-chat-skye")
+var lengthOfLongestSubstring = function(s) {
+    if(s.length ==0){return 0}
+    let max=1,cur=s[0],check;
+  for(let i = 1 ;i < s.length;i++){
+      check=cur.indexOf(s[i]);
+        if(check>=0){
+          if(cur.length>max){
+              max=cur.length;
+          }
+          cur=cur.slice(check+1);
+        }
+        cur+=s[i];
+  }
+  if(cur.length>max){
+    max=cur.length;
+  }
+  return max;
+};
+console.log(lengthOfLongestSubstring('asdsadsadwh1hdhiu1heo2'))
